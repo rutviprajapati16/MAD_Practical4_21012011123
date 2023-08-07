a748ec44fe6a0414da897f485f9d47a4f0e20f91
Student <student@gnu.ac.in>
2023-08-07 08:55:56 +0530

    Practical4

---
+++ app/src/main/java/com/example/mad_practical4_21012011123/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical4_21012011123
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
